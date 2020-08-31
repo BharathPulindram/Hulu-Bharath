05680c254d4c03c7b30147ca730e980e09777fe4
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-31 22:54:39 +0530

    first

---
+++ src/components/VideoCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./VideoCard.css";
+function VideoCard() {
+  return (
+    <div className="videoCard">
+      <img
+        alt=""
+        src="https://i.pinimg.com/originals/cc/18/8c/cc188c604e58cffd36e1d183c7198d21.jpg"
+      />
+      <p>This is film abt....</p>
+      <h2>Movie name </h2>
+      <p>Likes !!</p>
+    </div>
+  );
+}
+
+export default VideoCard;
