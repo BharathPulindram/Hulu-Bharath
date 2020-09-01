9d536a00d3fdc1ad8d9efe744110f24886884c2f
Bharath Kumar <pbkbharath1707@gmail.com>
2020-09-01 11:02:59 +0530

    Updates

---
+++ precache-manifest.4d7595e01c939833bbd7537cd4f9fcbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d3d15fe3346723f94b73b1bb8ed659",
+    "url": "/Hulu-Bharath/index.html"
+  },
+  {
+    "revision": "727b72a7f37897b01524",
+    "url": "/Hulu-Bharath/static/css/main.30edf3b1.chunk.css"
+  },
+  {
+    "revision": "22ab09cd6ad87b0501f7",
+    "url": "/Hulu-Bharath/static/js/2.8b9e511e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Hulu-Bharath/static/js/2.8b9e511e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727b72a7f37897b01524",
+    "url": "/Hulu-Bharath/static/js/main.f79c65a5.chunk.js"
+  },
+  {
+    "revision": "cdd6f4111d03aa305832",
+    "url": "/Hulu-Bharath/static/js/runtime-main.992440af.js"
+  }
+]);
